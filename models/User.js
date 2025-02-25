35f0bf51bbcae8f8514fe3c94cfdc1851910ba3f
wogur-yang <100479yjh@koreatech.ac.kr>
2025-02-25 20:29:19 +0900

    bcryptjs2

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
@@ -11,8 +11,8 @@ const userSchema = new mongoose.Schema({
 // ✅ 비밀번호 해싱 (저장 전 암호화)
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
     next();
 });
 
