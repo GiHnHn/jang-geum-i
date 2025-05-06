b585a8ee789ca317389e9e01a1e13e56321aaa02
GiHnHn <gksrlgjs0767@gmail.com>
2025-05-06 21:24:27 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -412,7 +412,7 @@ app.get('/api/search', async (req, res) => {
 
 
 const TTS_SERVER_MAP = {
-    baek:  "https://ms-calculators-plant-duo.trycloudflare.com/tts",
+    baek:  "https://painted-graham-newspapers-refugees.trycloudflare.com/tts",
     seung: "https://seung-tts.example.com/tts",
     jang:  "https://jang-tts.example.com/tts",
   };
